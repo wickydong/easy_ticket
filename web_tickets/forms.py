64cdd85f91ef20145d00d330fe7ba5ec443e4aff
wickydong <i@sihaizi.com>
2015-11-01 21:53:47 +0800

    嘻嘻

--- web_tickets/forms.py
+++ web_tickets/forms.py
@@ -1,29 +1,44 @@
 # -*- coding: utf-8 -*-
 from django import forms
-
+from web_tickets.models import *
 
 class LoginForm(forms.Form):
     user = forms.EmailField(max_length=30,error_messages={"required":"用户名不能为空"})
     passwd = forms.CharField(max_length=30,min_length=5,\
             widget=forms.PasswordInput,error_messages={"required":"密码不能为空"})
-class ServerForm(forms.Form):
+class ServerForm(forms.ModelForm):
+    class Meta:
+        model = Server
+        fields = ("server_ip","needtime","needother",)
     server_ip = forms.CharField(max_length=30,label="服务器IP:")
     needtime = forms.CharField(max_length=30,label="需要时长:")
     needother = forms.CharField(label="申请用途:",widget=forms.Textarea)
-class UpgradeForm(forms.Form):
+class UpgradeForm(forms.ModelForm):
+    class Meta:
+        model = Upgrade
+        fields = ("upgrade","howtime","up_msg","up_error",)
     upgrade = forms.CharField(max_length=100,label="升级项目:")
     howtime = forms.CharField(max_length=30,label="升级时间:")
     up_msg = forms.CharField(label="升级内容:",widget=forms.Textarea)
     up_error = forms.CharField(label="影响\措施:",widget=forms.Textarea)
-class VirtualForm(forms.Form):
+class VirtualForm(forms.ModelForm):
+    class Meta:
+        model = Virtual
+        fields = ("virtual","system","needconf",)
     virtual = forms.CharField(max_length=30,label="用途:")
     system = forms.CharField(max_length=30,label="系统:")
     needconf = forms.CharField(max_length=30,label="硬件配置:",widget=forms.Textarea)
-class FixForm(forms.Form):
+class FixForm(forms.ModelForm):
+    class Meta:
+        model = Fix
+        fields = ("fix","howtime","fix_msg",)
     fix = forms.CharField(max_length=30,label="BUG类型:")
     howtime = forms.CharField(max_length=30,label="修复时间:")
     fix_msg = forms.CharField(label="修复内容:",widget=forms.Textarea)
-class UpdateForm(forms.Form):
+class UpdateForm(forms.ModelForm):
+    class Meta:
+        model = Update
+        fields = ("update","howtime","update_msg",)
     update = forms.CharField(max_length=30,label="更新的项目:")
     howtime = forms.CharField(max_length=30,label="更新时间:")
     update_msg = forms.CharField(label="更新内容梗概:",widget=forms.Textarea)
--- web_tickets/views.py
+++ web_tickets/views.py
@@ -5,8 +5,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
-from web_tickets.models import UserAuth,Tickets
-from web_tickets.forms import LoginForm,ServerForm,UpgradeForm,VirtualForm,UpdateForm,FixForm
+from web_tickets.models import *
+from web_tickets.forms import *
 
 
 def index(request):
@@ -33,45 +33,55 @@ def login_form(request):
         form = LoginForm()
     return render_to_response("login.html",{"form": form},\
                   context_instance=RequestContext(request))
+    
 
 def my_tickets(request):
    if request.session.get("status",False):
-        user_title = request.session.get("god")
+        user = request.session.get("user")
+        #user_title = request.session.get("god")
         if request.method == "POST":      #GET请求展示工单,POST请求处理工单
-            if user_title == "yes":
-                pass
-            else:
-                pass
+            pass 
+            #if user_title == "yes":
+            #    pass
+            #else:
+            #    pass
         else:
-            if user_title == "yes":
-                tickets = Tickets.objects.filter(tks_status="nyet")
-            else:
-                tickets = Tickets.objects.filter(user=request.session.get("user"))
-            return render_to_response("my_tickets.html",{"tickets": tickets})
+            #if user_title == "yes":
+            #    tickets = Tickets.objects.filter(tks_status="nyet")
+            #else:
+            server = Server.objects.filter(make_user=user)
+            upgrade = Upgrade.objects.filter(make_user=user)
+            virtual = Virtual.objects.filter(make_user=user)
+            fix = Fix.objects.filter(make_user=user)
+            update = Update.objects.filter(make_user=user)
+            return render_to_response("my_tickets.html",{"server":server,"upgrade":upgrade,\
+                                      "virtual":virtual,"fix":fix,"update":update})
 
 def tickets(request,t_type):
+    type_form = {
+                 "server": ServerForm,
+                 "upgrade": UpgradeForm,
+                 "virtual": VirtualForm,
+                 "update": UpdateForm,
+                 "fix": FixForm,
+                }
+    try:
+        t_type = str(t_type)
+    except ValueError:
+        raise Http404()
+    if request.session.get("status",False) and request.method == "POST":
+        form = type_form[t_type](request.POST)
+        if form.is_valid():
+            form.make_user = "a"
+            form.save()
+            return HttpResponseRedirect("/my_tickets/")
     if request.session.get("status",False) and request.method == "GET":
-        try:
-            t_type = str(t_type)
-        except ValueError:
-            raise Http404()
-        if t_type == "server":
-            form = ServerForm()
-        if t_type == "upgrade":
-            form = UpgradeForm()
-        if t_type == "virtual":
-            form = VirtualForm()
-        if t_type == "update":
-            form = UpdateForm()
-        if t_type == "fix":
-            form = FixForm()
-        return render_to_response("send_tickets.html",{"form":form},\
-                                   context_instance=RequestContext(request))
+        form = type_form[t_type]()
+    return render_to_response("send_tickets.html",{"form":form,"t_type":t_type},\
+                              context_instance=RequestContext(request))
+        
 
 
 
 
-
-            
-         
     
